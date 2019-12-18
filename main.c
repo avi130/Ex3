49fbcb71515f17fc9e1ffbe1c6de8fd35d4e270c
avi130 <57074327+avi130@users.noreply.github.com>
2019-12-18 18:09:02 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
+#include "find.h"
 int main() {
 
     char q[]="cat b\n\nthis is a text file crat\nlooking for the word cat\nthe program should print also cats\nand crats and lcat but it shouldent\nprint the word caats";
     print_similar_words(q);
-    //print_lines(q);
+
 
 }

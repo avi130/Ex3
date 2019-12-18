b38f50899afa18e4afc45d4b692db3a70a08a4d7
avi130 <57074327+avi130@users.noreply.github.com>
2019-12-19 00:55:00 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -2,9 +2,13 @@
 #include "find.h"
 #include <string.h>
 int main() {
+
+/*
     char w[WORD];
     char option;
-    getWord(w);
+    getLine(w);
+    printf("%s\n",w);
+
     option=getchar();
     printf("%s\n",w);
     printf("%c",option);
@@ -18,6 +22,30 @@ int main() {
     }
     if(option=='b')
     {
+        print_similar_words(w);
+    }
+
+*/
+    char line[LINE];
+    char w[WORD];
+    fgets(line,LINE , stdin);
+    int j = 0;
+    int i = 0;
+    for( i = 0 ; i<LINE ; i++){
+        if(line[i] == ' ' || line[i] == '\t' ){
+            break;
+        }
+        else{
+            w[j] = line[i];
+            j++;
+        }
+    }
+    w[j] = '\0';
+    i++;
+    if(line[i] == 'a'){
+        print_lines(w);
+    }
+    if(line[i] == 'b'){
         print_similar_words(w);
     }
     return 0;

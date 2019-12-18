16ee659620332da73b6faa0690ecc3f1d57c9230
avi130 <57074327+avi130@users.noreply.github.com>
2019-12-18 19:47:35 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -4,11 +4,11 @@ int main() {
 
     //char q[]="cat b\n\nthis is a text file crat\nlooking for the word cat\nthe program should print also cats\nand crats and lcat but it shouldent\nprint the word caats";
     //print_similar_words(q);
-
+/*
 
         char w[WORD];
         getword(w);
-        printf("%c",w);char c;
+        printf("%s",w);char c;
         printf("please enter your command. a / b ");
         scanf(" %c", &c);
         printf("%c",c);
@@ -20,8 +20,18 @@ int main() {
 
         else
             printf("there is no such commend.");
-
-        return 0;
+*/
+    char s[WORD];
+    char ch;
+    getword(s);
+    scanf(" %c",&ch);
+    if(ch == 'a'){
+        print_lines(s);
+    }
+    if(ch == 'b'){
+        print_similar_words(s);
+    }
+    return 0;
 
 
 }
